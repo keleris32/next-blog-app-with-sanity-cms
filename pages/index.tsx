d9c8256ca51fb1de2fabc0f8edecaac1e78c9f3e
keleris32 <charles.ejiegbu@gmail.com>
2022-03-10 22:52:41 +0100

    feat: moved Hero section to components and added Posts section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { InferGetServerSidePropsType, NextPage } from 'next';
 import Head from 'next/head';
+import { Hero, Posts } from '../components';
 import Header from '../components/Header';
 import { sanityClient } from '../config/sanity';
 import { Post } from '../typings';
@@ -8,9 +8,7 @@ interface Props {
   posts: [Post];
 }
 
-const Home: NextPage<Props> = ({
-  posts,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home = ({ posts }: Props) => {
   console.log('Postssss', posts);
   return (
     <div className="mx-auto max-w-7xl">
@@ -20,28 +18,8 @@ const Home: NextPage<Props> = ({
       </Head>
       <Header />
       <main>
-        {/* Hero Section */}
-        <div className="flex items-center justify-between border-y border-black bg-blue-300 py-16  px-2 sm:px-5 md:py-24 lg:py-32">
-          <div className="space-y-5">
-            <h1 className="max-w-xl font-serif text-4xl sm:text-5xl">
-              <span className="text-violet-900 underline decoration-violet-900 decoration-wavy decoration-2">
-                Radium
-              </span>{' '}
-              is a place to read, write and connect
-            </h1>
-            <h2>
-              It's free and easy to post your thoughts on any topic and connect
-              with millions of readers
-            </h2>
-          </div>
-          <div>
-            <img
-              className="hidden h-32 md:inline-flex lg:h-full lg:pr-20"
-              src="/favicon.png"
-              alt="Hero section logo"
-            />
-          </div>
-        </div>
+        <Hero />
+        <Posts posts={posts} />
       </main>
     </div>
   );
@@ -59,6 +37,7 @@ export const getServerSideProps = async () => {
         image,
       },
       mainImage,
+      body
     }
   `;
 
