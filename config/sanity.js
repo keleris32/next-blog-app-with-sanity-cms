6c8dc9e99fb82821723da0a2320030ae7aac1808
keleris32 <charles.ejiegbu@gmail.com>
2022-03-10 22:51:44 +0100

    feat: added @sanity/image-url to grab image

--- config/sanity.js
+++ config/sanity.js
@@ -1,8 +1,5 @@
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient,
-} from 'next-sanity'
+import { createCurrentUserHook, createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
   /**
@@ -21,17 +18,17 @@ export const config = {
    * Authenticated request (like preview) will always bypass the CDN
    */
   useCdn: process.env.NODE_ENV === 'production',
-}
+};
 
 // Set up the client for fetching data in the getProps page functions
-export const sanityClient = createClient(config)
+export const sanityClient = createClient(config);
 
 /**
  * Set up a helper function for generating Image URLs with only the asset
  * reference data in your documents.
  * Read more: https://www.sanity.io/docs/image-url
  */
-export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
 
 // Helper function for using the current logged in user account
-export const useCurrentUser = createCurrentUserHook(config)
+export const useCurrentUser = createCurrentUserHook(config);
